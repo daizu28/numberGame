a725411c701eee6ab8a9ca784a2d68c52d17232f
Reina Kobayashi <kinokodaizukinako@gmail.com>
2020-09-23 21:39:08 +0900

    Initial Commit

---
+++ numberGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  numberGame
+//
+//  Created by 小林玲衣奈 on 2020/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
